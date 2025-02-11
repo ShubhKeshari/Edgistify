f411e2f11c2dc499732491a4a4fe3627cd87bb6d
ShubhKeshari <shubh1999keshari@gmail.com>
2025-02-11 20:11:54 +0530

    Backend api modified

--- e-commerce-frontend/src/services/api.js
+++ e-commerce-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 //const API_URL = 'http://localhost:8080/api';
-const API_URL = "https://book-bazaar-backend-yyf6.onrender.com"
+const API_URL = "https://book-bazaar-backend-yyf6.onrender.com/api"
 const api = axios.create({
   baseURL: API_URL,
   headers: {
